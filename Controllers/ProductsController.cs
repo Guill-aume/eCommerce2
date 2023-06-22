73364e8813414d3e42c1e7b3f8e1b76fc02b3296
anoop <anujasasikumar@gmail.com>
2023-06-22 11:29:46 -0400

    Product and order

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using eCommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace eCommerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Products.ToList();
+            return View(data);
+        }
+    }
+}
+
+
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Descripition")]
+        public string Description { get; set; }
+        [Display(Name = "ImageURL")]
+        public string ImageURl { get; set; }
+        [Display(Name = "Price")]
+        public float Price { get; set; }
+    }
+}
